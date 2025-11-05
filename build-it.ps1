d04fe7067af2287806766ba91e7893d25f639954
Guga-CL <gustavo.clima@yahoo.com.br>
2025-11-04 21:52:17 -0300

    ok

--- build-it.ps1
+++ build-it.ps1
@@ -1,3 +1,13 @@
+function ErrorCheck {
+    if ($LASTEXITCODE -ne 0) {
+    return #throw "dotnet build failed with exit code $LASTEXITCODE"
+}
+    
+}
 dotnet restore
+
 dotnet build src\My.Custom.Section.Plugin\My.Custom.Section.Plugin.csproj -c Release
+ErrorCheck
+
 dotnet build MyCustomJellyfinSection.sln -c Release
+ErrorCheck
